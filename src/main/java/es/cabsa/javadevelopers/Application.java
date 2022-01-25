dd2d1735f8f0b02ed74028d10d5309af006050b9
Daniel <dbenitez@uoc.edu>
2022-01-25 09:00:47 +0100

    Añadida Base de datos en memoria [Simplificaión prueba]

--- src/main/java/es/cabsa/javadevelopers/Application.java
+++ src/main/java/es/cabsa/javadevelopers/Application.java
@@ -1,9 +1,18 @@
 package es.cabsa.javadevelopers;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.h2.tools.Server;
 
 @SpringBootApplication
 public class Application {
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,7 +5,7 @@ CREATE table jungle.animal
   id   INT,
   name VARCHAR(50),
   legs INT,
-
+  food INT,
   PRIMARY KEY (id)
 );
 CREATE table jungle.food
@@ -16,13 +16,13 @@ CREATE table jungle.food
 );
 
 
-INSERT INTO jungle.animal VALUES ('0', 'Rabbit', '4');
-INSERT INTO jungle.animal VALUES ('1', 'Bear', '4');
-INSERT INTO jungle.animal VALUES ('2', 'Deer', '4');
-INSERT INTO jungle.animal VALUES ('3', 'Snake', '0');
-INSERT INTO jungle.animal VALUES ('4', 'Coocodrile', '4');
-INSERT INTO jungle.animal VALUES ('5', 'Chicken', '2');
-INSERT INTO jungle.animal VALUES ('6', 'Spider', '8');
+INSERT INTO jungle.animal VALUES ('0', 'Rabbit', '4','0');
+INSERT INTO jungle.animal VALUES ('1', 'Bear', '4','1');
+INSERT INTO jungle.animal VALUES ('2', 'Deer', '4','2');
+INSERT INTO jungle.animal VALUES ('3', 'Snake', '0','4');
+INSERT INTO jungle.animal VALUES ('4', 'Coocodrile','4','4');
+INSERT INTO jungle.animal VALUES ('5', 'Chicken', '2','5');
+INSERT INTO jungle.animal VALUES ('6', 'Spider', '8','3');
 
 
 INSERT INTO jungle.food VALUES ('0', 'carrot');
