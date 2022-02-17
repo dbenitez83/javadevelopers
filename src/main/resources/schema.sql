410a70c78ad6654de996cb66fca2d0f057570aff
Daniel <dbenitez@uoc.edu>
2022-02-17 07:04:13 +0100

    Añadida Base de datos en memoria [Simplificaión prueba]

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,40 +5,54 @@ CREATE table foods
   PRIMARY KEY (id)
 );
 
+CREATE table animalFamily
+(
+    id INT,
+    name VARCHAR(50),
+     PRIMARY KEY (id)
+);
+
 CREATE table animals
 (
   id   INT,
   name VARCHAR(50),
   legs INT,
   idFood INT,
+  idFamily INT,
+  FOREIGN KEY (idFamily) REFERENCES animalFamily(id),
   FOREIGN KEY (idFood) REFERENCES foods(id),
   PRIMARY KEY (id)
 );
 
-CREATE table excluidosEstudio
+CREATE table studyExcluded
 (
-  idAnimalExcluido   INT,
-  PRIMARY KEY (idAnimalExcluido)
+  idAnimalExcluded   INT,
+  PRIMARY KEY (idAnimalExcluded)
 );
 
+INSERT INTO animalFamily VALUES(0,'mammal');
+INSERT INTO animalFamily VALUES(1,'reptile');
+INSERT INTO animalFamily VALUES(2,'birds');
+INSERT INTO animalFamily VALUES(3,'arthropod');
+
 INSERT INTO foods VALUES (0, 'carrot');
-INSERT INTO foods VALUES (1, 'honey');
-INSERT INTO foods VALUES (2, 'leafs');
-INSERT INTO foods VALUES (3, 'insects');
-INSERT INTO foods VALUES (4, 'vermin');
-INSERT INTO foods VALUES (5, 'birdseed');
+INSERT INTO foods VALUES (1, 'leafs');
+INSERT INTO foods VALUES (2, 'insects');
+INSERT INTO foods VALUES (3, 'vermin');
+INSERT INTO foods VALUES (4, 'birdseed');
 
-INSERT INTO animals VALUES (0, 'Rabbit', 4,0);
-INSERT INTO animals VALUES (1, 'Bear', 4,1);
-INSERT INTO animals VALUES (2, 'Deer', 4,2);
-INSERT INTO animals VALUES (3, 'Snake', 0,4);
-INSERT INTO animals VALUES (4, 'Coocodrile',4,4);
-INSERT INTO animals VALUES (5, 'Chicken', 2,5);
-INSERT INTO animals VALUES (6, 'Spider', 8,3);
-
+INSERT INTO animals VALUES (0, 'Rabbit', 4,0,0);
+INSERT INTO animals VALUES (1, 'Ape', 2,1,0);
+INSERT INTO animals VALUES (2, 'Deer', 4,1,0);
+INSERT INTO animals VALUES (3, 'Snake', 0,2,1);
+INSERT INTO animals VALUES (4, 'Coocodrile',4,3,1);
+INSERT INTO animals VALUES (5, 'Chicken', 2,4,2);
+INSERT INTO animals VALUES (6, 'Spider', 8,2,3);
 
 
 
-INSERT INTO excluidosEstudio VALUES (3);
+
+INSERT INTO studyExcluded VALUES (3);
+
 
 
